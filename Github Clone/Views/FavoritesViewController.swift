a42e5134e1076bfc65095285723db8679b8e10b0
Umar Akram <uamirad00@gmail.com>
2025-03-23 02:16:17 +0200

    First Commit

---
+++ Github Clone/Views/FavoritesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesViewController.swift
+//  Github Clone
+//
+//  Created by Umar Akram on 02/02/2025.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
